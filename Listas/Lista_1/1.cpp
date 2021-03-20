457546b2a949d7c89d5bf056c447db3ab837507c
Patrick Matos <patrickmatos9@gmail.com>
2021-03-20 19:20:08 -0300

    Resolver o ex 1

--- Listas/Lista_1/1.cpp
+++ Listas/Lista_1/1.cpp
@@ -1,5 +1,12 @@
-#include <stdio.h>
+include <stdio.h>
+int main {
 
-int main() {
-    printf("Hello, World!");
+    float lado_cubo;
+
+    printf("Insira o lado do cubo: ");
+    scanf("%f", &lado_cubo);
+
+    float area = lado_cubo * lado_cubo * lado_cubo;
+
+    printf("O lado do cubo é %.2f", lado_cubo);    
 }
